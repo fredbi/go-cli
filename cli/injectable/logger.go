05b3c20da4472ccd53e1ba4d4df23084bb50b1d6
fredbi <fredbi@yahoo.com>
2023-11-02 09:40:37 +0100

    added injectable wrapper for zap.Logger (#4)
    Signed-off-by: Frederic BIDON <fredbi@yahoo.com>

--- cli/injectable/logger.go
+++ cli/injectable/logger.go
@@ -3,16 +3,31 @@ package injectable
 import (
 	"context"
 	"log/slog"
+
+	"go.uber.org/zap"
 )
 
-var _ ContextInjectable = &SLogger{}
+var (
+	_ ContextInjectable = &SLogger{}
+	_ ContextInjectable = &ZapLogger{}
+)
 
-type SLogger struct {
-	logger *slog.Logger
-}
+type (
+	SLogger struct {
+		logger *slog.Logger
+	}
+
+	ZapLogger struct {
+		logger *zap.Logger
+	}
+)
 
 func NewSLogger(l *slog.Logger) *SLogger {
 	return &SLogger{logger: l}
+}
+
+func NewZapLogger(l *zap.Logger) *ZapLogger {
+	return &ZapLogger{logger: l}
 }
 
 func (c *SLogger) Context(ctx context.Context) context.Context {
@@ -28,9 +43,22 @@ func (c SLogger) FromContext(ctx context.Context) interface{} {
 	return logger
 }
 
-// SLoggerFromContext retrieves a configuration registry from the context.
+func (c *ZapLogger) Context(ctx context.Context) context.Context {
+	return context.WithValue(ctx, ctxSLogger, c.logger)
+}
+
+func (c ZapLogger) FromContext(ctx context.Context) interface{} {
+	logger, ok := ctx.Value(ctxSLogger).(*slog.Logger)
+	if !ok {
+		return nil
+	}
+
+	return logger
+}
+
+// SLoggerFromContext retrieves a standard structured logger from the context.
 //
-// Optional defaulters can be added to deal with a non-existing config.
+// Optional defaulters can be added to deal with a non-existing logger.
 func SLoggerFromContext(ctx context.Context, defaulters ...func() *slog.Logger) *slog.Logger {
 	var c SLogger
 
@@ -42,4 +70,20 @@ func SLoggerFromContext(ctx context.Context, defaulters ...func() *slog.Logger) 
 	}
 
 	return logger.(*slog.Logger)
+}
+
+// ZapLoggerFromContext retrieves a zap logger from the context.
+//
+// Optional defaulters can be added to deal with a non-existing logger.
+func ZapLoggerFromContext(ctx context.Context, defaulters ...func() *slog.Logger) *zap.Logger {
+	var c ZapLogger
+
+	logger := c.FromContext(ctx)
+	if logger == nil {
+		for _, defaulter := range defaulters {
+			logger = defaulter()
+		}
+	}
+
+	return logger.(*zap.Logger)
 }
